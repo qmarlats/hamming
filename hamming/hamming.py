7523738dddbbd234fe967ed815ac345f20793380
Quentin Marlats <quentin.marlats@qmarlats.com>
2017-05-18 13:55:13 +0200

    Add 'get_m' function

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -13,3 +13,17 @@ def get_n(message):
         The total number of bits
     """
     return 2**(ceil(log(len(message))/log(2)) + 1) - 1
+
+
+def get_m(message):
+    """
+    Get the number of parity bits needed for a message in order to be encoded
+    with a Hamming code.
+
+    Arguments:
+        - message (str): the binary message
+
+    Returns:
+        The number of parity bits
+    """
+    return ceil(log(len(message))/log(2)) + 1
